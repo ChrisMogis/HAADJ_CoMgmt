1d7f6a6aae8e457b6fc706a3fb2f2e7baa0c51ad
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2023-12-06 15:57:51 +0100

    Update HAADJ_CoMgmt_Ckeck.ps1

--- HAADJ_CoMgmt_Ckeck.ps1
+++ HAADJ_CoMgmt_Ckeck.ps1
@@ -1,26 +1,35 @@
 <#
 .NOTES
-    Version       : 1.0.0
+    Version       : 1.0.3
     Author        : Christopher Mogis
-    Creation Date : 15/09/2023
-	
+    Creation Date : 30/10/2023
+    
 .DESCRIPTION
 
-	V1.0.0
-	Ce script permet de verifier l'état des composants nécessaires pour :
-		- L'hybridation AD et Azure AD
-		- Activation du Co-Management SCCM / MS Intune
-		- Etat du Windows Defender
-		- Etat du firewall Windows et des régles appliquées sur le poste de travail
-	Des que toutes les composants sont opérationnels, un rapport au format CSV est edité puis envoyé au service IT.
-	L'utilisateur recoit de son coté une notification au format Toast.
-	
+    V1.0.0
+    Ce script permet de verifier l'état des composants nécessaires pour :
+        - L'hybridation AD et Azure AD
+        - Activation du Co-Management SCCM / MS Intune
+        - Etat du Windows Defender
+        - Etat du firewall Windows et des régles appliquées sur le poste de travail
+    Des que toutes les composants sont opérationnels, un rapport au format CSV est edité puis envoyé au service IT.
+    L'utilisateur recoit de son coté une notification au format Toast.
+
+    V1.0.1
+    Corrections diverses
+
+    V1.0.2
+    Optimisation du script
+
+    V1.0.3
+    Ajout d'un tag dans le registre pour le controle d'execution lorsque le poste est hors connexion
+    
 #>
 
 #Declaration des variables
 $DeviceName = $(Get-WmiObject Win32_Computersystem).name
 $logfilepath = "C:\Temp\HAADJ_CoMan_$Devicename.csv"
-$LogShare = "\\nas.domain.lan\Share$\Logs_Compliance_Script"
+$LogShare = "\\wp063nas0001.commun01.svc\logpdt$\Mowe_Logs_Compliance_Script"
 $Continue = $True
 
 #Declaration de la fonction Toast
@@ -52,16 +61,18 @@ $notify = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifi
 $notify.Show($ToastXml)
 }
 
-#Detection du fichier contenant les resultats du check 
-$TestPathCSV = Test-Path "$($LogShare)\HAADJ_CoMan_$Devicename.csv"
-If ($TestPathCSV -ne "True")
+#Detection registre pour utilisateur connecté hors site ou deconnecté
+$RegKeyHaadj = "HKCU:\\Software\\HAADJCheck"
+$RegKeyHaadjName = "HAADJResult"
+$ScriptHAADJResult = (Get-Item $RegKeyHaadj -EA Ignore).Property -contains $RegKeyHaadjName
+if ($ScriptHAADJResult -ne "True")
 {
 
 #Execution du script
 While($Continue) {
 
     Start-Sleep -s 300
-	
+    
     #Variables
     $Date = Get-Date
     $Domain = dsregcmd /status
@@ -83,74 +94,74 @@ While($Continue) {
     #Device Domain Join verification
     $AD = $Domain | Where-Object {$_ -like "*DomainJoined : Yes*"}
     $ResultAD = if ($AD) {"Yes"}else{"No"}
-	#Write-Host "Verification de la connectivité à l'AD" -ForegroundColor Yellow
+    #Write-Host "Verification de la connectivité à l'AD" -ForegroundColor Yellow
     
     #Search Device Azure AD Joined
     $AAD = $Domain | Where-Object {$_ -like "*AzureAdJoined : Yes*"}
     $ResultAAD = if ($AAD) {"Yes"}else{"No"}
-	#Write-Host "Verification de la connectivité à l'AzureAD" -ForegroundColor Yellow
+    #Write-Host "Verification de la connectivité à l'AzureAD" -ForegroundColor Yellow
 
     #Verification de l'existance de la cle JoinInfo
     $JoinInfo = Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin\JoinInfo"
-	#Write-Host	"Récupération de la valeur de registre JoinInfo" -ForegroundColor Yellow
+    #Write-Host "Récupération de la valeur de registre JoinInfo" -ForegroundColor Yellow
     if ($JoinInfo -eq "True")
-		{
+        {
         $RJoinInfo = "Yes"
-		}
+        }
     else 
-		{
+        {
         $RJoinInfo = "No"
-		}
-	
+        }
+    
     #Verification de l'existance de la cle DeviceClientID
     $regKeyPath01 = "HKLM:\\SOFTWARE\Microsoft\Provisioning\OMADM\MDMDeviceID"
     $regValueName01 = "DeviceClientId"
     $DeviceClientID = (Get-Item $regKeyPath01 -EA Ignore).Property -contains $regValueName01
-	#Write-Host	"Récupération de la valeur de registre DeviceID" -ForegroundColor Yellow
+    #Write-Host "Récupération de la valeur de registre DeviceID" -ForegroundColor Yellow
     if ($DeviceClientID -eq "True")
-    {
+		{
         $RDeviceClientID = "Yes"
-    }
+		}
     else 
-    {
+		{
         $RDeviceClientID = "No"    
-    }	
+		}   
     
     #Verification de l'agent SCCM
     $SCCMService = (Get-Service -Name CcmExec).Status
-	#Write-Host	"Récupération du status du service SCCM" -ForegroundColor Yellow
+    #Write-Host "Récupération du status du service SCCM" -ForegroundColor Yellow
     if ($SCCMService -eq "Running")
-		{
+        {
         $RSCCMService = "Yes"
-		}
+        }
     else 
-		{
+        {
         $RSCCMService = "No"  
-		}
-	
+        }
+    
     #Verification gestion Microsoft Intune
     $INTUNEService = (Get-Service -Name IntuneManagementExtension).Status
-	#Write-Host	"Recuperation du status du service MS Intune" -ForegroundColor Yellow
+    #Write-Host "Recuperation du status du service MS Intune" -ForegroundColor Yellow
     if ($INTUNEService -eq "Running")
-		{
+        {
         $RIntuneService = "Yes"
-		}
+        }
     else 
-		{
+        {
         $RIntuneService = "No"  
-		}
-	
+        }
+    
     #Verification de la presence du certificat MS Intune
-    #Write-Host	"Recuperation du status de certificat Intune" -ForegroundColor Yellow
+    #Write-Host "Recuperation du status de certificat Intune" -ForegroundColor Yellow
     $CertDetailIntune = Get-ChildItem -Path 'Cert:\LocalMachine\My' -Recurse
     $CertDetailIntune | Select-Object @{n="Issuer";e={(($_.Issuer -split ",") |? {$_ -like "CN=*"}) -replace "CN="}}
     $ResultCertDetailIntune = if ($CertDetailIntune -like "*Intune*") {"Yes"}else{"No"}
-	
-	#Verification du status Azure PRT
+    
+    #Verification du status Azure PRT
     #Write-Host "Verification du status d'Azure PRT" -ForegroundColor Yellow
-	$AADPRT = $Domain | Where-Object {$_ -like "*AzureAdPrt : YES*"}
+    $AADPRT = $Domain | Where-Object {$_ -like "*AzureAdPrt : YES*"}
     $ResultAADPRT = if ($AADPRT) {"Yes"}else{"No"}
-	
+    
     #Verification Windows Defender
     #Write-Host "Récupération des informations de Microsoft Defender" -ForegroundColor Yellow
     #Service Microsoft Defender Antivirus Service
@@ -167,52 +178,52 @@ While($Continue) {
     $wscsvcService = Get-Service wscsvc
     $wscsvcServiceStatus = ($wscsvcService).Status
     if ($wscsvcServiceStatus -eq 'Running') {$RwscsvcServiceService = "Yes"}else{$RwscsvcServiceService = "No"}
-
+    
     #Verification Windows Firewall
     $fw = Get-NetFirewallRule -PolicyStore ActiveStore -PolicyStoreSource MDM | Sort-Object DisplayName | Select-Object Name
-	#Write-Host "Récupération du status du Firewall" -ForegroundColor Yellow
+    #Write-Host "Récupération du status du Firewall" -ForegroundColor Yellow
     $fwrule = $fw.Count
     if ($fwrule -ge "85")
-    {
+		{
         $Resultfw = 'Yes'
-    }
+		}
     else 
-    {
+		{
         $Resultfw = 'No'
-    }
+		}
 
     #Check HAADJ
     #Write-Host "Check HAADJ" -ForegroundColor Yellow
     If ($ResultAD -eq 'Yes' -AND $ResultAAD -eq 'Yes' -AND $RJoinInfo -eq 'Yes' -AND $RDeviceClientID -eq 'Yes' -AND $ResultCertDetailMSOrganization -eq 'Yes' -AND $ResultAADPRT -eq 'Yes')
-    {
+		{
         $HAADJ = 'Yes'
-    }
+		}
     else 
-    {
+		{
         $HAADJ = 'No'
-    }
+		}
     
     #Check Co-Management
     #Write-Host "Check Co-Management" -ForegroundColor Yellow
     If ($RSCCMService -eq 'Yes' -AND $RINTUNEService -eq 'Yes' -AND $ResultCertDetailIntune -eq 'Yes')
-    {
+		{
         $CoMgnt = 'Yes'
-    }
+		}
     else 
-    {
+		{
         $CoMgnt = 'No'
-    }
+		}
     
     #Check Windows Defender and Windows firewall
     #Write-Host "Check Firewall et MS Defender" -ForegroundColor Yellow
     If ($RDefService -eq 'Yes' -AND $RSecurityHealthService -eq 'Yes' -AND $RwscsvcServiceService -eq 'Yes' -AND $Resultfw -eq 'Yes')
-    {
+		{
         $WinDef = 'Yes'
-    }
+		}
     else 
-    {
+		{
         $WinDef = 'No'
-    }
+		}
 
     #Report
     $report = New-Object psobject
@@ -236,12 +247,33 @@ While($Continue) {
 
 #Notification pour l'admin
 If ($HAADJ -eq 'Yes' -and $CoMgnt -eq 'Yes' -and $WinDef -eq 'Yes')
-
     {
-    #Write-Host "Envoie des résultats au service IT" -ForegroundColor Yellow
-    Copy-Item $logfilepath -Destination $LogShare -Force
-    balloon "Bonjour $env:UserName" "Votre poste $Devicename est pret !" 10000
-	($Continue = $false)
+    try {
+        #Creation de l'objet HAADJCheck dans le registre
+        New-Item –Path "HKCU:Software" –Name "HAADJCheck"
+        #Write-Host "Envoie des résultats au service IT" -ForegroundColor Yellow
+        Copy-Item $logfilepath -Destination $LogShare -Force
+	    #Creation d'un tag dans le registre
+	    New-ItemProperty -Path "HKCU:\\Software\\HAADJCheck" -Name "HAADJResult" -Value "OK"  -PropertyType "ExpandString"
+    	}
+    catch
+        {
+        Write-host "Erreur de copie, réexecution du script"
+        }
     }
-	}
+    
+#Detection du fichier contenant les resultats du check 
+    $TestPathCSV = Test-Path "$($LogShare)\HAADJ_CoMan_$Devicename.csv"
+    If ($TestPathCSV -eq "True")
+        {
+        #Notification Utilisateur final 
+        balloon "Bonjour $env:UserName" "Votre poste $Devicename est pret !" 10000
+        #Arret de la boucle
+        ($Continue = $false)
+        }
+    else
+        {
+        Write-Host "Error copie de fichier"
+        }
+    }
 }
